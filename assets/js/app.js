cff8239bfadfe2a67f2c4a67a6e37a11411f4c38
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-04-26 17:30:21 +0600

    get user email and password

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,10 @@
 
-const handleUser = event => {
-    event.preventDefault();
-}
+document.getElementById('login-submit').addEventListener('click', function(){
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+})
